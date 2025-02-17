98bbe18d02a9d30caf1417ac0e85de7ec84cce8d
Yogesh Shinde <yogeshshinde3624@gmail.com>
2025-02-17 22:18:40 +0530

    Updated folder strcture

---
+++ src/layouts/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProfileInfo from '../components/Profile/ProfileInfo'
+
+function ProfilePage() {
+  return (
+    <>
+    <ProfileInfo/>
+    </>
+  )
+}
+
+export default ProfilePage
