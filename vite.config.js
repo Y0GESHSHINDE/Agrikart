b8a01a9894bcf5303ea8c81486fdd7b0859251d1
Nirbhay <nirbhaymarde43@gmail.com>
2025-03-15 21:15:55 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",  
+    fs:{
+      strict: false,  
+    },
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
